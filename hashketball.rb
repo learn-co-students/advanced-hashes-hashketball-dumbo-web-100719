e9777f66cc4acc7efaed189eb70ffefe17d46836
Brianna Fetterley <bnfetterley@usfca.edu>
2019-10-01 11:41:10 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -14,7 +14,7 @@
      },
      {:player_name => "Bismack Biyombo",
      :number => 0,
-     :show => 16,
+     :shoe => 16,
      :points => 12,
      :rebounds => 4,
      :assists => 7,
@@ -22,7 +22,17 @@
      :blocks => 15,
      :slam_dunks => 10
      },
-     
+     {:player_name => "DeSagna Diop",
+     :number => 2,
+     :shoe => 14,
+     :points => 24,
+     :rebounds => 12,
+     :assists => 12,
+     :steals => 4,
+     :blocks => 5,
+     :slam_dunks => 5 
+     },
+     {
 
 
 
